8e949cd96619a23fe42785d5e7e093057cb4bda5
yankai3306 <yankai@nauapp.com>
2020-09-23 18:20:44 +0800

    add toString method

--- lib/cookie.dart
+++ lib/cookie.dart
@@ -12,4 +12,9 @@ class Cookie{
   bool secure = false;
 
   Cookie(this.name, this.value);
+
+  @override
+  String toString() {
+    return 'Cookie{name: $name, value: $value, expires: $expires, maxAge: $maxAge, domain: $domain, path: $path, httpOnly: $httpOnly, secure: $secure}';
+  }
 }
